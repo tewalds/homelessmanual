5f1e2a719ab396d2c3cb227bec1dfc6f32b377ef
Derek <derek@ubuntu.(none)>
2011-02-12 17:03:55 -0600

    index.php edited for step.php

--- www/index.php
+++ www/index.php
@@ -39,6 +39,9 @@ $router->add("GET", "/editprofile",   "profile.php", "editprofile",   'user',  n
 $router->add("POST","/updateprofile", "profile.php", "updateprofile", 'user',  array("name" => "string", "organization" => "string", "bio" => "string"));
 
 
+$router->add("GET", "/viewstep",       "step.php",    "viewstep",     'user',  array("id" => "int");
+$router->add("POST","/editstep",       "step.php",    "edit",         'user',  array("id" => "int");
+
 $route = $router->route();
 
 $user = auth(def($_COOKIE['session'], ''));
