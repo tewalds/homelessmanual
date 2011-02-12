37b1cfb8524a8c9bc337aec1123d7d2743d97ed0
Derek <derek@ubuntu.(none)>
2011-02-12 17:11:41 -0600

    updated viewstep.php and step.php

--- www/templates/viewstep.php
+++ www/templates/viewstep.php
@@ -9,21 +9,22 @@ $QuestionTitle = "a bank account";
 <table border="0" width="100%">
 	<tr>
 		<td colspan="2">
-		 <h2><?php echo $QuestionType?> <?php echo $QuestionTitle?>?</h2>
+		 <h2><?php echo $step['category'] ?>: <?php echo $step['title'] ?></h2>
 		</td>
 	</tr>
 	<tr><td width="150"></th>
-		<td width="70%"><b>Steps</b></th>
+		<td width="70%"><b>Steps:</b></th>
 		<td><b>Completed</b></th>
 	</tr>
-	<tr>
-		<td><button>expand</button></td><td>1.<a href="">Do you have a Step 1 Title</a></td><td><input type="checkbox" alt=></td>
-	</tr>
-	<tr>
-		<td><button>expand</button></td><td>2.<a href="">Do you have a Step 2 Title</a></td><td><input type="checkbox" alt=></td>
-	</tr>
-	<tr>
-		<td><button>expand</button></td><td>3.<a href="">Do you have a Step 3 Title</a></td><td><input type="checkbox" alt=></td>
+	<tr><?
+	 	
+	 	$counter = 0;	
+	 	
+		foreach($statement)
+		{?>
+			<td><button>expand</button></td><td>1.<a href="">Do you have a <? echo $statement[$counter, ']  Step 1 Title</a></td><td><input type="checkbox" alt=></td>
+		}
+		
 	</tr>
 </table>
 
