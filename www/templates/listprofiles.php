631dfa34d887f06294787e67a8d318331098b818
Timo Ewalds <timo@ewalds.ca>
2011-02-12 15:08:13 -0700

    Add profile listing, fix some profile viewing

--- www/templates/listprofiles.php
+++ www/templates/listprofiles.php
@@ -1,50 +1,21 @@
 
-
-<?php
-$email = "rcheramy@gmail.com";
-$username="Reg Cheramy";
-$company="Edistorm";
-$bio="Founder of Edistorm, creator of Helping Manual";
-
-$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=40"
-?>
-
 Search for Profile: <input type="text">
 <!-- searches based on username/organization/ -->
 
 <h2>Results</h2>
 <table border="0" width="100%">
+<? foreach($users as $user){ ?>
 	<tr>
 		<td width="40">
-		<img src="<?= $grav_url ?>" alt="" />
+		<img src="<?= gravurl($user['email']) ?>" alt="" />
 		
 		</td>
 		<td width="100%">
-			<span class="profile-name"><a href="/viewprofile?id=nnn"><?= $username?></a></span>
-			<span class="profile-company"><?=$company?></span>
+			<span class="profile-name"><a href="/viewprofile?id=<?= $user['id'] ?>"><?= $user['name'] ?></a></span>
+			<span class="profile-company"><?= $user['organization'] ?></span>
 			<span class="profile-type">Caseworker/Client/Admin</span>
 		</td>
 	</tr>
-	<tr>
-		<td width="40">
-		<img src="<?= $grav_url ?>" alt="" />
-		
-		</td>
-		<td width="100%">
-			<span class="profile-name"><a href="/viewprofile?id=nnn"><?= $username?></a></span>
-			<span class="profile-company"><?=$company?></span>
-			<span class="profile-type">Caseworker/Client/Admin</span>
-		</td>
-	</tr>
-	<tr>
-		<td width="40">
-		<img src="<?= $grav_url ?>" alt="" />
-		
-		</td>
-		<td width="100%">
-			<span class="profile-name"><a href="/viewprofile?id=nnn"><?= $username?></a></span>
-			<span class="profile-company"><?=$company?></span>
-			<span class="profile-type">Caseworker/Client/Admin</span>
-		</td>
-	</tr>
+<? } ?>
+
 </table>
