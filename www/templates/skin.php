a0c5e6470fe60ac92529ee31e9c729d976efce28
Macbook Reg <regcheramy@uwsclient-142-126.uws.ualberta.ca>
2011-02-12 14:57:30 -0700

    Header Logo

--- www/templates/skin.php
+++ www/templates/skin.php
@@ -1,3 +1,5 @@
+<?php $menu=array("home" => ""); ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -12,7 +14,7 @@
 <div id="wrapper">
 <!-- start header -->
 <div id="logo">
-	<h1><a href="#">Emporium </a></h1>
+	<h1><a href="#"><img src=/images/helpinglogo.png"></a></h1>
 	<h2> &raquo;&nbsp;&nbsp;&nbsp;Design by Free Css Templates</h2>
 </div>
 <div id="header">
