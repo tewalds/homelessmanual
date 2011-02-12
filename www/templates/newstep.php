5744c4385c55d327363bc2f005b79139d2d7f358
Macbook Reg <regcheramy@uwsclient-142-126.uws.ualberta.ca>
2011-02-12 16:37:02 -0700

    New Step File Created

---
+++ www/templates/newstep.php
@@ -0,0 +1,14 @@
+
+
+<table border="0" width="100%">
+	<tr>
+		<td colspan="2">
+		 <h2>Enter your question:</h2>
+		 
+		 <h2>
+		<select><option>How do I get</option><option>Where do I find</option><option>Who do I contact to</option></select><input size="100" type="text" value="">
+		 </h2>
+		</td>
+	</tr>
+
+</table>
