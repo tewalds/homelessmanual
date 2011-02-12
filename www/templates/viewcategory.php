ee874215ae15143177314ef7da8988b3d061860b
Macbook Reg <regcheramy@uwsclient-142-126.uws.ualberta.ca>
2011-02-12 13:03:04 -0700

    View Category and View Question Added

---
+++ www/templates/viewcategory.php
@@ -0,0 +1,20 @@
+
+<table border="0" width="100%">
+	<tr>
+		<td colspan="2">
+		 <h1>Category</h1>
+		</td>
+	</tr>
+	<tr>
+		<td width="70%"><a href="">Question Title</a></td>
+	</tr>
+	<tr>
+		<td width="70%"><a href="">Question Title</a></td>
+	</tr>
+	<tr>
+		<td width="70%"><a href="">Question Title</a></td>
+	</tr>
+	<tr>
+		<td width="70%"><a href="">Question Title</a></td>
+	</tr>
+</table>
---
+++ www/templates/viewquestion.php
@@ -0,0 +1,45 @@
+
+<?php
+$QuestionType = "How do I get";
+$QuestionTitle = "a bank account";
+?>
+
+
+
+<table border="0" width="100%">
+	<tr>
+		<td colspan="2">
+		 <h2><?php echo $QuestionType?> <?php echo $QuestionTitle?>?</h2>
+		</td>
+	</tr>
+	<tr><td width="150"></th>
+		<td width="70%"><b>Steps</b></th>
+		<td><b>Completed</b></th>
+	</tr>
+	<tr>
+		<td><button>expand</button></td><td>1.<a href="">Do you have a Step 1 Title</a></td><td><input type="checkbox" alt=></td>
+	</tr>
+	<tr>
+		<td><button>expand</button></td><td>2.<a href="">Do you have a Step 2 Title</a></td><td><input type="checkbox" alt=></td>
+	</tr>
+	<tr>
+		<td><button>expand</button></td><td>3.<a href="">Do you have a Step 3 Title</a></td><td><input type="checkbox" alt=></td>
+	</tr>
+</table>
+
+<table border="0" width="100%">
+	<tr>
+		<td colspan="2">
+		 <h2>Discussion</h2>
+		</td>
+	</tr>
+	<tr>
+		<td>Discussion around the current question<td>usename</td><td>date</td>
+	</tr>
+	<tr>
+		<td>Discussion around the current question<td>usename</td><td>date</td>
+	</tr>
+	<tr>
+		<td>Discussion around the current question<td>usename</td><td>date</td>
+	</tr>
+</table>
