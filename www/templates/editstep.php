97ce13efa7b6044a1c0b20fb7eec0592aa872675
Macbook Reg <regcheramy@uwsclient-142-126.uws.ualberta.ca>
2011-02-12 19:38:55 -0700

    checking in broken code

--- www/templates/editstep.php
+++ www/templates/editstep.php
@@ -1,14 +1,43 @@
+<script>
+ 	
+$(function() {  
+  $('.error').hide();  
+  $(".button").click(function() {  
+    // validate and process form here  
+  
+    $('.error').hide();  
+      var name = $("input#title").val();  
+        if (name == "") {  
+      $("label#title_error").show();  
+      $("input#title").focus();  
+      return false;  
+    }  
+	var dataString = 'name='+ title + '&email=' + category;  
+	alert (dataString);return false;  
 
+	$.ajax({  
+	  type: "POST",  
+	  url: "/updatestep.php",  
+	  data: dataString,  
+	  success: function() {  
+	      alert("submitted");
+	    });  
+	  }  
+	});  
+	return false;    
+  });  
+});  
+</script>
 
 <table border="0" width="100%">
 	<tr>
 		<td colspan="2">
 		 <h2>Edit your question:</h2>
-		 <form method="post" action="/updateestep">
+		 <form name="namecat">
 		 <input size="30" type="text" name="title" value="<?= htmlentities($step['title']) ?>">
 		 <select name="category"><option value="0"> Category</option><?= make_select_list_key($categories, $step['category']) ?></select>
 		<br>
-		<input type="submit" value="Update Step">
+		<input type="submit" class="button" value="Update Step">
 		</form>
 		</td>
 	</tr>
