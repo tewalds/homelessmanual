cf0ba575117f163c7b84b4d23b4f9f6fd61e3a40
Derek <derek@ubuntu.(none)>
2011-02-13 17:39:28 -0600

    fixed aboutus.php again

--- www/templates/aboutus.php
+++ www/templates/aboutus.php
@@ -9,39 +9,39 @@ In 2009, the City of Edmonton mandated to end homelessness by 2029. Helping Manu
 
 Startup Weekends are 54-hour events where developers, coders, designers, marketers, product managers and startup enthusiasts come together to share ideas, form teams, build products, and create start-ups. Startup Weekends are weekend-long, hands-on experiments where entrepreneurs and aspiring entrepreneurs can find out if startup ideas are viable.  Most events have about 75 attendees, half of which have technical backgrounds and the other half have business backgrounds.
 
-Helping Manual contributors:
+<big>Helping Manual contributors:</big>
 
-Reg Cheramy - Founder of Edistorm a cloud based brainstorming tool. (Check out www.edistorm.com)
+<b>Reg Cheramy</b> - Founder of Edistorm a cloud based brainstorming tool. (Check out www.edistorm.com)
 Reg is excited about the prospects of streamlining the exchange of knowledge that Helping Manual can bring to the homeless and affordable housing space in Edmonton. Reg contributed with the overall vision, project management, some CSS, some broken javascript, hacky tables based layout and a pretty funky logo.
 
-Timo Ewalds
+<b>imo Ewalds</b>
 Lead programmer extraordinaire, Timo was the programming glue that held the team together.
 
-Anne Matthews
+<b>Anne Matthews</b>
 Anne is the founder of Healthy House. Her passion for ending homelessness in Edmonton contributed to the overall vision and initial brainstorming.
 
-Will Zaichkowski
+<b>Will Zaichkowski</b>
 Will has been in the IT/ICT business for 15 years, largely working as an analyst, architect and in business development. He brainstormed with Reg a couple of times on this idea, and during Startup Weekend he worked on the content and data modeling side of the project.
 
-Derek Dowling
+<b>Derek Dowling</b>
 Derek is an NFL fan who enjoys Computers, and good causes. He worked on this project as a junior programmer.
 
-Kael Nicholson
+<b>Kael Nicholson</b>
 Kael is an analyst and project manager for iomer. He worked on database content and structure.
 
-Scott Montgomerie
+<b>Scott Montgomerie</b>
 Scott is a jQuery wizard who came in and threw in his two cents (which is worth a heck of a lot more) on this project. 
 
-Tammy Borowiecki
+<b>Tammy Borowiecki</b>
 Tammy has a marketing and communications background and an interest in being part of a startup. She contributed to the research and content for this project.
 
-Veronica Izquierdo
+<b>Veronica Izquierdo</b>
 Veronica has a civil engineering background. She participated in the group for the real purpose of startup weekend, to learn how a project is done.
 
-Jason Hammermeister
+<b>Jason Hammermeister</b>
 Jason's background is in HR consulting. He joined this project to see what could be built in a weekend. He contributed to the research and content for this project.
 
-Bhushan Mulpuri
+<b>Bhushan Mulpuri</b>
 Bhushan works in the health industry. He contributed to the research and content development for this project.
 
 </p>
