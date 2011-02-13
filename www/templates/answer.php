94ca70967f4a8b90a58b3f5ec4d16cda0aaa6f77
Derek <derek@ubuntu.(none)>
2011-02-13 13:58:48 -0600

    revision of answer.php

--- www/templates/answer.php
+++ www/templates/answer.php
@@ -5,16 +5,17 @@
 		 <h2>Answer a Question</h2>
 		</td>
 	</tr>
-<? foreach($categories as $category){ ?>
+<? foreach($categories as $catid => $cattitle){ ?>
 	<tr>
-		<td width="70%"><a href="/viewcategory?id=<?= $category['id'] ?>"><?= $category['title'] ?></a></td>
+		<td width="70%"><a href="/viewcategory?id=<?= $catid ?>"><?= $cattitle ?></a></td>
 	</tr>
 	<? foreach($unanswered as $answer) { ?>
 	<tr>
-	 	<? if($answer['category'] == $category) { ?>
-	 		<td><a href=""><? $answer['title'] ?></a></td>
+	 	<? if($answer['category'] == $catid) { ?>
+	 		<td><a href="/viewstep?id=<?= $answer['id'] ?>"><?= $answer['title'] ?></a></td>
 	 	<? } ?>
 	</tr>
+	<? } ?>
 <? } ?>
 </table>
 
