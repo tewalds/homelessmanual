2bfc723832aeaf80793e1153bc30fad6c9d840e3
Derek <derek@ubuntu.(none)>
2011-02-13 17:28:15 -0600

    added aboutus.php

---
+++ www/templates/aboutus.php
@@ -0,0 +1,47 @@
+<h2>What we're all about: <h2>
+<br> <br>
+<p>
+
+ "The fact is that with cooperative systems, a single person can now contribute to solve a problem, without having to move into a professional
+relationship with an institution. This is a profound change in the ways human affairs are arranged." - Clay Shriky
+
+In 2009, the City of Edmonton mandated to end homelessness by 2029. Helping Manual: A guide for homeless and affordable housing is an independent private sector initiative built to facilitate access to relevant information, and collect the knowledge contributions of anyone who is willing to help end homelessness. It started when Anne Matthews, Reg Cheramy, Timo Ewalds and Sean Healy met over drinks to flush out the idea of using technology as a tool to leverage the efforts of countless case workers who are currently working on helping people who are in need of support, in a homeless or low-income situation. The idea was brought to life at a "Startup Weekend" event.
+
+Startup Weekends are 54-hour events where developers, coders, designers, marketers, product managers and startup enthusiasts come together to share ideas, form teams, build products, and create start-ups. Startup Weekends are weekend-long, hands-on experiments where entrepreneurs and aspiring entrepreneurs can find out if startup ideas are viable.  Most events have about 75 attendees, half of which have technical backgrounds and the other half have business backgrounds.
+
+Helping Manual contributors:
+
+Reg Cheramy - Founder of Edistorm a cloud based brainstorming tool. (Check out www.edistorm.com)
+Reg is excited about the prospects of streamlining the exchange of knowledge that Helping Manual can bring to the homeless and affordable housing space in Edmonton. Reg contributed with the overall vision, project management, some CSS, some broken javascript, hacky tables based layout and a pretty funky logo.
+
+Timo Ewalds
+Lead programmer extraordinaire, Timo was the programming glue that held the team together.
+
+Anne Matthews
+Anne is the founder of Healthy House. Her passion for ending homelessness in Edmonton contributed to the overall vision and initial brainstorming.
+
+Will Zaichkowski
+Will has been in the IT/ICT business for 15 years, largely working as an analyst, architect and in business development. He brainstormed with Reg a couple of times on this idea, and during Startup Weekend he worked on the content and data modeling side of the project.
+
+Derek Dowling
+Derek is an NFL fan who enjoys Computers, and good causes. He worked on this project as a junior programmer.
+
+Kael Nicholson
+Kael is an analyst and project manager for iomer. He worked on database content and structure.
+
+Scott Montgomerie
+Scott is a jQuery wizard who came in and threw in his two cents (which is worth a heck of a lot more) on this project. 
+
+Tammy Borowiecki
+Tammy has a marketing and communications background and an interest in being part of a startup. She contributed to the research and content for this project.
+
+Veronica Izquierdo
+Veronica has a civil engineering background. She participated in the group for the real purpose of startup weekend, to learn how a project is done.
+
+Jason Hammermeister
+Jason's background is in HR consulting. He joined this project to see what could be built in a weekend. He contributed to the research and content for this project.
+
+Bhushan Mulpuri
+Bhushan works in the health industry. He contributed to the research and content development for this project.
+
+</p>
