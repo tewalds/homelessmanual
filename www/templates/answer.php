9e06f4f5357dbdd18237289f80eb78044d9b4db0
Macbook Reg <regcheramy@air.(none)>
2011-02-13 11:22:00 -0700

    Answer Page

---
+++ www/templates/answer.php
@@ -0,0 +1,20 @@
+
+<table border="0" width="100%">
+	<tr>
+		<td colspan="2">
+		 <h2>Answer a Question</h2>
+		</td>
+	</tr>
+<? foreach($categories as $category){ ?>
+	<tr>
+		<td width="70%"><a href="/viewcategory?id=<?= $category['id'] ?>"><?= $category['title'] ?></a></td>
+	</tr>
+	<!-- repeat for each question in this category -->
+	<tr>
+	 	<td><a href="">Question Title</a></td>
+	</tr>
+	<!-- end repeat for each question in this category -->
+	
+<? } ?>
+</table>
+
