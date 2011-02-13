2c60634ad74cfe805f66290789b607b945ce8d51
Timo Ewalds <timo@ewalds.ca>
2011-02-12 21:02:12 -0700

    Switch from dos file endings to linux file endings, make the search go to a reasonable place

--- www/templates/skin.php
+++ www/templates/skin.php
@@ -58,11 +58,9 @@ foreach($menu as $name => $link)
 ?>
 			<li id="search">
 				<h2>Search</h2>
-				<form method="get" action="">
-					<fieldset>
-					<input type="text" id="s" name="s" value="" />
-					<input type="submit" id="x" value="Search" />
-					</fieldset>
+				<form method="get" action="/searchsteps">
+				<input type="text" id="q" name="q" value="" />
+				<input type="submit" id="x" value="Search" />
 				</form>
 			</li>
 		</ul>
