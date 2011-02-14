24e48e5a6b4b42fefcc451a3f44f64b96f7ee473
Timo Ewalds <timo@ewalds.ca>
2011-02-13 17:42:29 -0700

    Minor bugfix

--- www/pages/step.php
+++ www/pages/step.php
@@ -103,7 +103,7 @@ function createsubquestion($data, $user) {
 	global $db;
 
 	if(!$data['substepid'])
-		$data['substepid'] = $db->pquery("SELECT id FROM steps WHERE title = ?", $data['subquestion'])->fetchrowset();
+		$data['substepid'] = $db->pquery("SELECT id FROM steps WHERE title = ?", $data['subquestion'])->fetchrow();
 
 	if($data['stepid'] && $data['substepid']){
 		$priority = $db->pquery("SELECT priority FROM steporder WHERE parentstepid = ? ORDER BY priority DESC LIMIT 1", $data['stepid'])->fetchrow();
